aae73033a40750d6827f5a3a15022b775a5028ad
yangck <chaokunyang@qq.com>
2017-08-21 10:42:56 +0800

    add jkes architecture diagram

---
+++ jkes-boot/spring-data-jpa/src/main/java/com/timeyang/jkes/spring/jpa/index/Queryer.java
@@ -0,0 +1,12 @@
+package com.timeyang.jkes.spring.jpa.index;
+
+/**
+ * Query data from database and add data to queue to be send to kafka by other send thread
+ *
+ * @author chaokunyang
+ */
+public class Queryer {
+
+
+
+}
